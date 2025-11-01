e20f6521cd1c6dc8d6101f286e20fdee8c75e356
germanusk <germanuskeming@gmail.com>
2025-11-01 15:43:45 +0100

    update change of program

--- app/Http/Controllers/Admin/ProgramController.php
+++ app/Http/Controllers/Admin/ProgramController.php
@@ -1336,18 +1336,23 @@ class ProgramController extends Controller
 
                 $year = substr(Batch::find(Helpers::instance()->getCurrentAccademicYear())->name, 2, 2);
                 $prefix = $program->prefix;//3 char length
+                $suffix = $program->suffix;//3 char length
                 $max_count = '';
                 if($prefix == null){
                     return back()->with('error', 'Matricule generation prefix not set.');
                 }
+                
                 $max_matric = json_decode($this->api_service->max_matric($prefix, $year))->data; //matrics starting with '$prefix' sort
+                // dd($max_matric);
                 if($max_matric == null){
                     $max_count = 0;
                 }else{
                     $max_count = intval(substr($max_matric, strlen($prefix)+4));
                 }
-                $next_count = substr('0000'.($max_count+1), -4);
-                $student_matric = $prefix.'/'.$year.'/'.$next_count;
+
+                // dd($max_count);
+                $next_count = substr('000'.(++$max_count), -4);
+                $student_matric = $prefix.$year.$suffix.$next_count;
 
                 if(ApplicationForm::where('matric', $student_matric)->count() == 0){
                     $data['title'] = "Change Student Program";
